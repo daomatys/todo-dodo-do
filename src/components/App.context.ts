7961d349a1c661299340edfe6ccd2472ee2207ef
Maxim Dejnew <red41tiger@mail.ru>
2022-01-26 16:58:33 +0700

    Resolved problems with AppContext entypization

--- src/components/App.context.ts
+++ src/components/App.context.ts
@@ -1,5 +1,24 @@
 import React from 'react';
+import ITaskBar from './TaskBar/TaskBar.interface';
+import DEFAULTS from './App.defaults';
 
-const AppContext = React.createContext({});
+interface IAppContext {
+  tasks: ITaskBar[],
+  tasksCount: number,
+  setTasks: (alpha:ITaskBar[]) => void,
+  setTasksCount: (beta:number) => void,
+}
+
+const alpha = DEFAULTS.tasks;
+const beta = DEFAULTS.count;
+
+const DefaultAppContext:IAppContext = {
+  tasks: alpha,
+  tasksCount: beta,
+  setTasks: (alpha) => { return },
+  setTasksCount: (beta) => { return },
+}
+
+const AppContext = React.createContext<IAppContext>(DefaultAppContext);
 
 export default AppContext;
